1a72773471c2e1b40196508385dcfbeac055c68d
IngGerardo <d.gerardo_64@hotmail.com>
2016-11-05 12:30:56 -0700

    template e-commerce

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('principal');
+});
+
 
 Auth::routes();
 
