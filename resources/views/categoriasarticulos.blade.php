52b50767246723e548d527eaa898a31b9f0fd6d1
oiecy2307 <osandoval710gmail.com>
2016-11-10 23:52:36 -0700

    lo puse otra vez

--- resources/views/categoriasarticulos.blade.php
+++ resources/views/categoriasarticulos.blade.php
@@ -12,7 +12,7 @@
 	<div class="col-md-4" align="center">
 		<div class="form-group">
 		<a href="" data-target="#dataPokemon" data-toggle="modal" data-id="{{ $a->id }}" data-descripcion="{{ $a->descripcion }}" data-nombrepokemon="{{ $a->precio }}" title="{{ $a->descripcion }}">
-			
+			<img src="../img/{{ $a->id }}.png" width="80%" class="img-responsive imagen carta"></a>
 		</div>
 	</div>
 	@endforeach
