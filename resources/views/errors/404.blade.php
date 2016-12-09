df5fabbb5b745aa1fc0b0cc08fa7f9c1018413d1
IngGerardo <d.gerardo_64@hotmail.com>
2016-12-09 08:56:41 -0700

    agregar 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dugout Barber</title>
+    <link rel="stylesheet" href="{{ asset("css404/style.css") }}">
+    <link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,200italic,200,300,300italic,400italic,600,600italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- For-Mobile-Apps-and-Meta-Tags -->
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Simple Error Page Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //For-Mobile-Apps-and-Meta-Tags -->
+</head>
+    <body>
+      <div class="row">
+           <div class="main w3l">
+                <div class="col-xs-12">
+                    <img src="{{ asset('images/Online-shop.jpg') }}" heigth="40%" width="40%" class="img-responsive">
+                    <h2><font color="black">¡ERROR!</font></h2>
+                    <h3><font color="black"> FAVOR DE CONTACTAR AL ADMINISTRADOR</font></h3>
+                    <a href="{{ url('/') }}" class="back"><h4><b>Regresar</b></h4></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
